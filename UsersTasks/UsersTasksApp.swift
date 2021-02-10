ccbbd2b915b0d0db299483aab4c2af1fc5e4a977
Alexandru <aual@gft.com>
2021-02-10 15:21:56 +0100

    Initial Commit

---
+++ UsersTasks/UsersTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersTasksApp.swift
+//  UsersTasks
+//
+//  Created by Apostol, Alexandru on 10/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct UsersTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
